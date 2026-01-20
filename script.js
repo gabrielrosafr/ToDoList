708a9cfc355d86d80ba831d4f92d6cfe9954777f
Gabriel Fernandes <gabrielfernandes@MacBook-Air-de-Gabriel.local>
2026-01-20 11:41:24 -0300

    adicionado escuta do botao e add tarefas

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ const input = document.getElementById("adicionarTarefa");
 const button = document.getElementById("addBotao");
 const list = document.getElementById("listaTarefas");
 
-console.log(input, button, list);
+button.addEventListener("click", function() {
+
+    const text = input.value;
+
+    if (text === "") return;
+
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    list.appendChild(li);
+
+    input.value = "";
+
+});
